bd81b2e34712c101344dea3b86bcc75fce7dab6f
Booplah <vincentt957@hotmail.com>
2025-12-09 23:29:10 -0500

    Timas Branch

--- Project/Scripts/Dashboard.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("toggleNCRList").addEventListener("click", function() {
-  const extraRows = document.getElementById("extraNCRs");
-  const isHidden = extraRows.classList.contains("hidden");
-  extraRows.classList.toggle("hidden");
-  this.textContent = isHidden ? "Hide NCRs ▲" : "View All NCRs ▼";
-});
